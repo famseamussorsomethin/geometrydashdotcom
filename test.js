2cc8054fd0eccce09e7f33914c2dbe3a81e998a5
lasokar <129457995+lasokar@users.noreply.github.com>
2026-04-04 17:50:38 -0700

    update music

--- test.js
+++ test.js
@@ -15,7 +15,12 @@ self.addEventListener('fetch', event => {
   console.log('request:', url.pathname);
   if (url.pathname.includes('1.txt')) {
     event.respondWith(
-      fetch(`/geometrydashdotcom/assets/${levelID}.txt`)
+      fetch(`/geometrydashdotcom/assets/levels/${levelID}.txt`)
+    );
+  }
+   if (url.pathname.includes('StereoMadness.mp3')) {
+    event.respondWith(
+      fetch(`/geometrydashdotcom/assets/music/${levelID}.mp3`)
     );
   }
 });
